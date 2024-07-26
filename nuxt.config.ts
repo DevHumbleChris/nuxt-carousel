88c45c8a6a4078e003d9a9578a3a18c071e87f05
DevHumbleChris <dev.humblechris254@gmail.com>
2024-07-26 11:03:39 +0300

    added shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  modules: [
+    "@nuxt/image",
+    "@vueuse/motion/nuxt",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  image: {
+    quality: 80,
+    format: ["webp"],
+  },
 });
